333565b930ab1e192cdad8cc9807c0041b9dab78
alexander-hp <hdezppalex@gmail.com>
2023-05-26 19:05:49 -0600

    Actualizando interfaz para dateSim

--- app/src/profileSettings/profileSettings.js
+++ app/src/profileSettings/profileSettings.js
@@ -1,10 +1,47 @@
-var showAvatars = false;
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+var showAvatars;
+var myAvatar = document.getElementById('imgAvatar');
+var currentAvatar = 'https://cdn-icons-png.flaticon.com/128/4086/4086652.png';
+document.getElementById('selectAvatar').style.display = 'none';
+(() => {
+  'use profileSettings.html';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
+
+function swipe() {
+  window.location.href = 'http://localhost/DateSim/app/src/swipe/swipe.html';
+}
 
 function showSelectAvatar() {
+  showAvatars = !showAvatars;
   if (showAvatars) {
     document.getElementById('selectAvatar').style.display = 'block';
+    location.href = '#selectAvatar';
   } else {
     document.getElementById('selectAvatar').style.display = 'none';
   }
-  showAvatars = !showAvatars;
+}
+
+function selectAvatar(urlAvatar) {
+  console.log(urlAvatar);
+  myAvatar.src = urlAvatar;
+  document.getElementById('selectAvatar').style.display = 'none';
 }
